3dbec00af3593c3fb1a9f60bba6eb62e8f031157
arthur <arthurazevedods@gmail.com>
2024-07-08 14:56:52 -0300

    disabled play and stop

--- src/components/Temporizador.vue
+++ src/components/Temporizador.vue
@@ -10,17 +10,20 @@
         data () {
             return {
                 tempoEmSegundos: 0, //estado
-                cronometro: 0
+                cronometro: 0,
+                cronometroRodando: false
             }
         },
         methods:{
             iniciar (){
                 //1seg = 1000ms
+                this.cronometroRodando = true;
                 this.cronometro = setInterval(() =>{
                     this.tempoEmSegundos += 1
                 },1000)
             },
             finalizar (){
+                this.cronometroRodando = false
                 clearInterval(this.cronometro)
             }
         }
@@ -30,13 +33,13 @@
 <template>
     <div class="is-flex is-align-items-center is-justify-content-space-between">
         <Cronometro :tempoEmSegundos="tempoEmSegundos"/>
-        <button class="button" @click="iniciar">
+        <button class="button" @click="iniciar" :disabled="cronometroRodando">
             <span class="icon">
                 <i class="fas fa-play"></i>
             </span>
             <span>play</span>
         </button>
-        <button class="button" @click="finalizar">
+        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
             <span class="icon">
                 <i class="fas fa-stop"></i>
             </span>
